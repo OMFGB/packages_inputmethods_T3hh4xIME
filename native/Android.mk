aab62bb2f423c6ee1765790e02ed29c229314b9c
r2DoesInc <r2doesinc@poiteeserver>
2011-05-20 23:27:22 -0400

    Resolve build conflicts with original LatinIME package.

--- native/Android.mk
+++ native/Android.mk
@@ -11,8 +11,8 @@ LOCAL_SRC_FILES := \
 LOCAL_NDK_VERSION := 4
 LOCAL_SDK_VERSION := 8
 
-LOCAL_MODULE := libjni_latinime
+LOCAL_MODULE := libjni_t3hh4xime
 
-LOCAL_MODULE_TAGS := user
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
