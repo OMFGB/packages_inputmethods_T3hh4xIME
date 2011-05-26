15d8da7980f3eacafd9bf0eff895f1d2cdd1a494
r2DoesInc <r2doesinc@gmail.com>
2011-05-25 21:43:17 -0400

    Try to fix this

--- native/Android.mk
+++ native/Android.mk
@@ -4,14 +4,14 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/src
 
 LOCAL_SRC_FILES := \
-	jni/com.t3hh4xx0r.inputmethod.t3hh4xime_BinaryDictionary.cpp \
+	jni/com_t3hh4xx0r_inputmethod_t3hh4xime_BinaryDictionary.cpp \
 	src/dictionary.cpp \
 	src/char_utils.cpp
 
 LOCAL_NDK_VERSION := 4
 LOCAL_SDK_VERSION := 8
 
-LOCAL_MODULE := libjni_t3hh4xime
+LOCAL_MODULE := libjni_latinime
 
 LOCAL_MODULE_TAGS := optional
 
